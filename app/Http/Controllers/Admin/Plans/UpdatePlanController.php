281490bed7062df5249de6f1d14bed189250d5a4
Jonne C C Silva <jonne.cley@gmail.com>
2023-05-06 11:38:34 -0300

    TD-14: controles criados

---
+++ app/Http/Controllers/Admin/Plans/UpdatePlanController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Plans;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UpdatePlanController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
