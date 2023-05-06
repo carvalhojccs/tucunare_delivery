adce2082da63a5c8412214f91c466d524a561bb5
Jonne C C Silva <jonne.cley@gmail.com>
2023-05-06 11:50:14 -0300

    TD-14: criado o controller ShowPlanContoller

---
+++ app/Http/Controllers/Admin/Plans/ShowPlanController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Plans;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ShowPlanController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
