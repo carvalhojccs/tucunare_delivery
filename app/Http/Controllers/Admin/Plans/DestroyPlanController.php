d24c579c9a0a4bff2563eada3d7a6e5e3fdb4f4a
Jonne C C Silva <jonne.cley@gmail.com>
2023-05-07 13:04:51 -0300

    TD-17: criada roitna delatar plano

--- app/Http/Controllers/Admin/Plans/DestroyPlanController.php
+++ app/Http/Controllers/Admin/Plans/DestroyPlanController.php
@@ -3,15 +3,20 @@
 namespace App\Http\Controllers\Admin\Plans;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Plans;
 
 class DestroyPlanController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke($url)
     {
-        //
+      Plans::where('url', $url)->delete();
+
+      //Plans::destroy($plan);
+      
+      return redirect()->route('plans.index');
+
     }
 }
--- resources/views/admin/pages/plans/show.blade.php
+++ resources/views/admin/pages/plans/show.blade.php
@@ -24,7 +24,7 @@
         <x-label>Última atualização</x-label>
         <x-input type="text" name="updated_at" value="{{ $plan->updated_at->format('d/m/Y - H:m:s') }}" disabled />
       @endif
-    <form>
+        </form>
 </div>
 <div
   class="block mt-2 rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700"
@@ -47,15 +47,16 @@
       </x-link>
     </div>
     
-    <x-form action="#" method="POST">
+    <x-form action="{{ route( 'plans.destroy', $plan->url ) }}" method="DELETE">      
       <x-button class="bg-red-500">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
           <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
         </svg>
         Deletar
       </x-button>
+    </x-form>
   </div>
-  </x-form>
+  
 
 </div>
 </x-app-layout>
