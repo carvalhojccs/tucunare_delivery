1eed2f65bac9ed5fcadb9eecb970a8985e4e76ee
Jonne C C Silva <jonne.cley@gmail.com>
2023-05-07 19:32:58 -0300

    filtro de pesquisa implementado

--- resources/views/admin/pages/plans/index.blade.php
+++ resources/views/admin/pages/plans/index.blade.php
@@ -18,6 +18,37 @@
       <div class="flex flex-col">
         <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+
+            <x-form action="{{ route('plans.search') }}" method="POST">
+              <div class="bg-white items-center justify-between w-full flex rounded-full shadow-lg p-2 mb-5 sticky" style="top: 5px">
+                <div>    
+                  <div class="p-2 mr-1 rounded-full hover:bg-gray-100 cursor-pointer">    
+                    <svg class="h-6 w-6 text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                      <path fill-rule="evenodd" d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z" clip-rule="evenodd" />
+                    </svg>    
+                  </div>    
+                </div>
+    
+                <input 
+                  class="font-bold uppercase rounded-full w-full py-4 pl-4 text-gray-700 bg-gray-100 leading-tight focus:outline-none focus:shadow-outline lg:text-sm text-xs" 
+                  type="text" 
+                  name="filter"
+                  placeholder="Pesquisar..."
+                  value="{{ $filters['filter'] ?? '' }}"
+                >
+    
+                
+                  <button class="bg-gray-600 p-2 hover:bg-blue-400 cursor-pointer mx-2 rounded-full">    
+                    <svg class="w-6 h-6 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                      <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
+                    </svg>    
+                  </button>    
+                
+                
+              </div>
+            </x-form>
+
+
             <div class="overflow-hidden">
               <table class="w-full text-left text-sm font-light">
                 <thead class="border-b font-medium dark:border-neutral-500">
@@ -52,7 +83,12 @@
     </div>
     <div
       class="border-t-2 border-neutral-100 px-6 py-3 dark:border-neutral-600 dark:text-neutral-50">
-      {!! $plans->links() !!}
+      @if (isset($filters))
+        {!! $plans->appends($filters)->links() !!}  
+      @else
+        {!! $plans->links() !!}
+      @endif
+      
     </div>
   </div>
 
