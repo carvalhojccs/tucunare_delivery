02c18744e69b65521475352469014fe061b22c7b
Jonne C C Silva <jonne.cley@gmail.com>
2023-05-14 16:11:56 -0300

    TD-33: criado os controles

---
+++ app/Http/Controllers/Admin/Permission/DestroyPermissionController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Permission;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DestroyPermissionController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
