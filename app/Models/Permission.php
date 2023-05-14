be656eb4693d7b33dec30874c81ece19bc81b8db
Jonne C C Silva <jonne.cley@gmail.com>
2023-05-14 15:32:15 -0300

    TD-29: criada e programada a model e a migration

---
+++ app/Models/Permission.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Permission extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description'
+    ];
+
+    public function search($filter = null)
+    {
+        return $this->where('name', 'ILIKE', "%{$filter}%")
+                    ->orWhere('description', 'ILIKE', "%{$filter}%")
+                    ->paginate();
+    }
+}
