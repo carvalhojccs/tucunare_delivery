8d540eea4ad6d496cfc5afe1dfa18ddc3e54b4aa
Jonne C C Silva <jonne.cley@gmail.com>
2023-05-14 22:18:51 -0300

    TD-39: relacionamentos programados

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Profile extends Model
 {
@@ -13,6 +14,11 @@ class Profile extends Model
         'name',
         'description'
     ];
+
+    public function permissions(): BelongsToMany
+    {
+        return $this->belongsToMany(Permission::class);
+    }
 
     public function search($filter = null)
     {
