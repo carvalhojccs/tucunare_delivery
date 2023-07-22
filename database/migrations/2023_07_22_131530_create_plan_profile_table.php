1cf6ff16eb2898343fef76b03481076176c03494
Jonne C C Silva <jonne.cley@gmail.com>
2023-07-22 10:16:53 -0300

    migrate created

---
+++ database/migrations/2023_07_22_131530_create_plan_profile_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Plan;
+use App\Models\Profile;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('plan_profile', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Plan::class);
+            $table->foreignIdFor(Profile::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('plan_profile');
+    }
+};
