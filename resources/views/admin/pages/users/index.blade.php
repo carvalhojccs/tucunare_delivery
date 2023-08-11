23f6a3a0c5ee8837a0443fbdf75979a8e4da692d
Jonne C C Silva <jonne.cley@gmail.com>
2023-08-10 21:31:52 -0300

    controller create created

---
+++ resources/views/admin/pages/users/index.blade.php
@@ -0,0 +1,7 @@
+<x-app-layout>
+    <x-container>
+        <x-breadcrumb>
+            <x-breadcrumb-active>{{ __('Usuários') }}</x-breadcrumb-active>
+        </x-breadcrumb>
+    </x-container>
+</x-app-layout>
