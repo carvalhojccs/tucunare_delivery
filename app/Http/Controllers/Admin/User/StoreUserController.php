76a7cb8d3fa951560e25bc96b6eed03d88dc2db2
Jonne C C Silva <jonne.cley@gmail.com>
2023-08-10 21:56:02 -0300

    store controller and store request updated

--- app/Http/Controllers/Admin/User/StoreUserController.php
+++ app/Http/Controllers/Admin/User/StoreUserController.php
@@ -3,15 +3,21 @@
 namespace App\Http\Controllers\Admin\User;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\User\StoreUserRequest;
+use App\Models\User;
 
 class StoreUserController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke(StoreUserRequest $request)
     {
-        //
+        $data = $request->all();
+        $data['tenant_id'] = auth()->user()->tenant_id;
+
+        User::create($data);
+
+        return to_route('users.index');
     }
 }
--- app/Http/Requests/User/StoreUserRequest.php
+++ app/Http/Requests/User/StoreUserRequest.php
@@ -22,7 +22,9 @@ class StoreUserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required', 'string','min:3', 'max:255'],
+            'email' => ['required', 'string', 'email', 'min:3', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:6', 'min:16'],
         ];
     }
 }
