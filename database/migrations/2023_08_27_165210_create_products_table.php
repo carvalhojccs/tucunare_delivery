4882c8ff1471906da544ed0e9d9e1a2a0390e941
Jonne C C Silva <jonne.cley@gmail.com>
2023-08-27 14:00:39 -0300

    model products and pivot writed

--- database/migrations/2023_08_27_165210_create_products_table.php
+++ database/migrations/2023_08_27_165210_create_products_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('title')->unique();
+            $table->string('slug')->unique();
+            $table->double('price',10,2);
+            $table->string('description');
             $table->timestamps();
         });
     }
