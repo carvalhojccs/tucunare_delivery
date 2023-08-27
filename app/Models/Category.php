b9e8220fbee6f648398771f53beb6b5c9f8116b3
Jonne C C Silva <jonne.cley@gmail.com>
2023-08-26 21:37:45 -0300

    model and migration writed

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'tenant_id',
+        'name',
+        'url',
+        'description'
+    ];
 }
--- database/migrations/2023_08_27_000130_create_categories_table.php
+++ database/migrations/2023_08_27_000130_create_categories_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Tenant;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,10 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Tenant::class)->constrained();
+            $table->string('name')->unique();
+            $table->string('url')->unique();
+            $table->string('description');
             $table->timestamps();
         });
     }
