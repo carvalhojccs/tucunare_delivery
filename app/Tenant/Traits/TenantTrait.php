0b0134f100e9f57d124c347f07bb601df5982c48
Jonne C C Silva <jonne.cley@gmail.com>
2023-08-27 00:55:01 -0300

    tenant global scope created

--- app/Tenant/Traits/TenantTrait.php
+++ app/Tenant/Traits/TenantTrait.php
@@ -3,11 +3,14 @@
 namespace App\Tenant\Traits;
 
 use App\Tenant\Observers\TenantObserver;
+use App\Tenant\Scopes\TenantScope;
 
 trait TenantTrait
 {
     protected static function booted()
     {
         static::observe(TenantObserver::class);
-    }
+
+        static::addGlobalScope(app(TenantScope::class));
+    }    
 }
