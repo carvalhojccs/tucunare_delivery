b1ef66b0e9e9c74316e49fa2560b892bd0825a4e
Jonne C C Silva <jonne.cley@gmail.com>
2023-08-26 21:43:11 -0300

    controllers created

---
+++ app/Http/Controllers/Admin/Category/IndexCategoryController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Category;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class IndexCategoryController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
