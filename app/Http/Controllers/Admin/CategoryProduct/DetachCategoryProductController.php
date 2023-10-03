1ef2a6ace46b24c7c51cfb0f8e5c8af9e46d7bdb
Jonne C C Silva <jonne.cley@gmail.com>
2023-10-02 21:50:02 -0300

    Controllers created

---
+++ app/Http/Controllers/Admin/CategoryProduct/DetachCategoryProductController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Admin\CategoryProduct;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DetachCategoryProductController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
